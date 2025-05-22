d37b4f457510086fd6ca6593291f5872e2f0a047
webxrahat <islammdrahat308@gmail.com>
2025-05-22 17:08:32 +0600

    taskList add

---
+++ doc/steps.md
@@ -0,0 +1,12 @@
+### Create componets
+
+- Header - Done
+- Footer - Done
+- Hero - Done
+- Task Lists
+- Add Task
+- Update
+- Delete
+- Favorite
+- Tag Generate
+- Search Task
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import TaskBoard from "./components/tasks/TaskBoard";
 
 function App() {
   return (
     <>
-      <div>My app</div>
+      <>
+        <Header />
+        <Hero />
+        <TaskBoard />
+        <Footer />
+      </>
     </>
   );
 }
